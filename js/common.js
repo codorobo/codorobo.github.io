1a8ee4f23e195bd38098176ea02b9dd849880156
Boris Bachurin <manager@codorobo.ru>
2021-05-10 11:02:15 +0300

    nav_add

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
-$(function() {
-
-	// Custom JS
-
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы    
+    PopUpHide();
 });
+//Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+}
